0cb74580cb485d37ec732a3a9ba03dff28b6642c
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-07-09 14:24:17 +0530

    esp32 cam firmware

---
+++ exploration-bot/cam_detection/cam.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import requests
+
+base_url = 'http://192.168.0.100:81'
+
+response = requests.get(base_url+"/stream", stream=True)
+buffer = bytes()
+
+for chunk in response.iter_content(chunk_size=1024):
+    buffer += chunk
+    a = buffer.find(b'\xff\xd8')
+    b = buffer.find(b'\xff\xd9')
+
+    if a != -1 and b != -1:
+        jpg = buffer[a:b+2]
+        buffer = buffer[b+2:]
+
+        # Check if jpg is not empty
+        if jpg:
+            # Convert the byte array to a numpy array
+            frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            
+            # Check if frame is not None (imdecode fails)
+            if frame is not None:
+                frame = cv2.flip(frame, 0)
+                
+                # Display the image
+                cv2.imshow('IP Camera Stream', frame)
+
+                key = cv2.waitKey(1)
+                if key == ord('q'):
+                    exit(0)
+            else:
+                print("Failed to decode frame.")
+        else:
+            print("Received empty image data.")
+
+cv2.destroyAllWindows()
+
