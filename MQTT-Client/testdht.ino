917bfc0d605e235e35b56b6eabb51a450878b0da
Naman Jain <114279102+LoneBub@users.noreply.github.com>
2024-07-10 23:40:23 +0530

    Add files via upload

---
+++ MQTT-Client/testdht.ino
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+void loop() {
+  // Wait a few seconds between measurements
+  
+  }
+
+  // Print results to the Serial Monitor
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+}
