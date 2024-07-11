cb4dd5aa28560bd2f0ab65d3ad2b726f0cadfd7f
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-07-11 12:12:01 +0530

    update ap

--- ap/ap.ino
+++ ap/ap.ino
@@ -1,6 +1,13 @@
 #include <WiFi.h>
-const char *ssid = "ESP32";
-const char *passphrase = "";
+#include "esp_wifi.h"
+#include "esp_wifi_types_generic.h"
+
+const char *soft_ap_ssid = "ESP32";
+const char *soft_ap_password = "";
+
+const char* wifi_network_ssid = "Pixel";
+const char* wifi_network_password =  "bqcpsfkt845j3vz";
+
 
 IPAddress local_IP(192,168,4,22);
 IPAddress gateway(192,168,4,9);
@@ -8,20 +15,24 @@ IPAddress subnet(255,255,255,0);
 
 void setup()
 {
-   Serial.begin(115200);
-  Serial.println();
+  WiFi.mode(WIFI_MODE_APSTA);
+
+  Serial.begin(115200);
+
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP(ssid,passphrase) ? "Ready" : "Failed!");
+  
+  Serial.print("Setting modes ... ");
+  WiFi.softAP(soft_ap_ssid, soft_ap_password);
+  //WiFi.begin(wifi_network_ssid, wifi_network_password);
   //WiFi.softAP(ssid);
   //WiFi.softAP(ssid, passphrase, channel, ssdi_hidden, max_connection)
-  
-  Serial.print("Soft-AP IP address = ");
+
+  Serial.print("ESP32 IP as soft AP: ");
   Serial.println(WiFi.softAPIP());
+ 
+  Serial.print("ESP32 IP on the WiFi network: ");
+  Serial.println(WiFi.localIP());  
 }
 void loop() {
-  Serial.print("[Server Connected] ");
-  Serial.println(WiFi.softAPIP());
-  delay(500);
 }
