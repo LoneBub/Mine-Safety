e48b4de5b01a7b708aa2d3bd329b4698dcefa1f1
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-07-24 22:18:29 +0530

    esp8266 ap update

--- sentry-tower/ap/ap.ino
+++ sentry-tower/ap/ap.ino
@@ -1,37 +1,47 @@
-#include <WiFi.h>
+#include <ESP8266WiFi.h>
 
 IPAddress local_IP(192,168,4,2);
 IPAddress gateway(192,168,4,1);
 IPAddress subnet(255,255,255,0);
 
-const char *soft_ap_ssid = "ESP32";
-const char *soft_ap_password = "";
+// Set AP credentials
+#define AP_SSID "Mine-Net"
+#define AP_PASS ""
 
-const char* wifi_network_ssid = "Modify";
-const char* wifi_network_password =  "isuq5478";
+// Set WiFi credentials
+#define WIFI_SSID "tp-link"
+#define WIFI_PASS "ITCUP-02"
+
 
 void setup()
 {
- // WiFi.mode(WIFI_MODE_APSTA);
+  WiFi.mode(WIFI_AP_STA);
 
   Serial.begin(115200);
 
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  
+
   Serial.print("Setting modes ... ");
-  WiFi.softAP(soft_ap_ssid, soft_ap_password);
-//  WiFi.begin(wifi_network_ssid, wifi_network_password);
-  //WiFi.softAP(ssid);
-  //WiFi.softAP(ssid, passphrase, channel, ssdi_hidden, max_connection)
+  
+  // Begin Access Point
+  WiFi.softAP(AP_SSID, AP_PASS);
+  
+  // Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 }
 
 void loop() {
-  Serial.print("ESP32 IP as soft AP: ");
-  Serial.println(WiFi.softAPIP());
- 
- // Serial.print("ESP32 IP on the WiFi network: "); 
- // Serial.println(WiFi.localIP());
+  Serial.print("IP address for network ");
+  Serial.print(WIFI_SSID);
+  Serial.print(" : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("IP address for network ");
+  Serial.print(AP_SSID);
+  Serial.print(" : ");
+  Serial.print(WiFi.softAPIP());
+  Serial.print("\n\n");
+
   delay(5000);  
 
 }
