0605f5245048e4dde5c3d7b34f9678820a9aa89c
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-07-28 16:58:05 +0530

    AP update

--- nodes/sentry-tower/ap/ap.ino
+++ nodes/sentry-tower/ap/ap.ino
@@ -8,14 +8,16 @@ IPAddress subnet(255,255,255,0);
 #define AP_SSID "Mine-Net"
 #define AP_PASS ""
 
+#define DEVICE 10
+
 // Set WiFi credentials
-#define WIFI_SSID "tp-link"
-#define WIFI_PASS "ITCUP-02"
+//#define WIFI_SSID ""
+//#define WIFI_PASS ""
 
 
 void setup()
 {
-  WiFi.mode(WIFI_AP_STA);
+  //WiFi.mode(WIFI_AP_STA);
 
   Serial.begin(115200);
 
@@ -25,21 +27,22 @@ void setup()
   Serial.print("Setting modes ... ");
   
   // Begin Access Point
-  WiFi.softAP(AP_SSID, AP_PASS);
+  WiFi.softAP(AP_SSID, AP_PASS,1,0,DEVICE);
   
   // Begin WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  //WiFi.begin(WIFI_SSID, WIFI_PASS);
 }
 
 void loop() {
   Serial.print("IP address for network ");
-  Serial.print(WIFI_SSID);
-  Serial.print(" : ");
-  Serial.println(WiFi.localIP());
-  Serial.print("IP address for network ");
   Serial.print(AP_SSID);
   Serial.print(" : ");
   Serial.print(WiFi.softAPIP());
+//  Serial.print("IP address for network ");
+//  Serial.print(WIFI_SSID);
+//  Serial.print(" : ");
+//  Serial.println(WiFi.localIP());
+
   Serial.print("\n\n");
 
   delay(5000);  
