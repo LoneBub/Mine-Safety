e98bd0da77ccc92e7ceccd2ec117c4c60d212c10
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-07-31 01:29:44 +0530

    exploration-bot-cam update

--- nodes/exploration-bot/firmware/firmware.ino
+++ nodes/exploration-bot/firmware/firmware.ino
@@ -1,53 +1,145 @@
+#define CAMERA_MODEL_AI_THINKER
+
 #include "esp_camera.h"
+#include "camera_pins.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <DHT.h>
+//#include <DHT_U.h>
 
-//#define SENSORS   // uncomment when you want to integrate sensors to node
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char *ssid = "tp-link";
+const char *password = "PITC'2023";
 
-IPAddress local_ip(192,168,4,3);
-IPAddress subnet(255,255,255,0);
+// Set your Static IP address
+IPAddress local_IP(192, 168, 0, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 0, 0);
 
-WiFiClient espClient;
+// Define sensor type and pin
+#define DHTPIN 1      // D15 
+#define DHTTYPE DHT11 // DHT11 sensor
+#define MQ2_PIN 3 //D13
+#define LED_BUILTIN 33
 
-PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "Mine-Net";   //Enter SSID WIFI Name
-const char* password = "";   //Enter WIFI Password
+// GPIO Settings
+int gpLb    =  2;  // Left 1
+int gpLf    = 14;  // Left 2
+int gpRb    = 15;  // Right 1
+int gpRf    = 13;  // Right 2
+int gpLed   =  4; // Light
 
-
-//  AI Thinker CAM pin config
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
-
-// GPIO Setting
-extern int gpLb =  2; // Left 1
-extern int gpLf = 14; // Left 2
-extern int gpRb = 15; // Right 1
-extern int gpRf = 13; // Right 2
-extern int gpLed =  4; // Light
+char* mq_value;
 String WiFiAddr ="";
 
-void startCameraServer();
+// MQTT broker address
+const char* mqtt_server = ("192.168.4.102");
+const int mqttPort = 1883;
 
+// Initializing ESP client
+WiFiClient espClient;
+// Initializing ESP as MQTT Client
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+
+void blink_led(unsigned int times, unsigned int duration)
+{
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(duration);
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(200);
+  }
+}
+
+void startCameraServer();
+void setupLedFlash(int pin);
+
+void connect_mqttServer() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+        //now attemt to connect to MQTT server
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect("ESP32_client3")) { // Change the name of client here if multiple ESP32 are connected
+          //attempt successful
+          Serial.println("connected");
+          // Subscribe to topics here
+          client.subscribe("rpi/broadcast");          
+        }
+        else {
+          //attempt not successful
+          Serial.print("failed, rc=");
+          Serial.print(client.state());
+          Serial.println(" trying again in 2 seconds");
+    
+          blink_led(3,200); //blink LED three times (200ms on duration) to show that MQTT server connection attempt failed
+          // Wait 2 seconds before retrying
+          delay(2000);
+        }
+  }
+  
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Check if a message is received on the topic "rpi/broadcast"
+  if (String(topic) == "rpi/broadcast") {
+      if(messageTemp == "10"){
+        Serial.println("Action: blink LED");
+        blink_led(1,1250); //blink LED once (for 1250ms ON time)
+      }
+  }
+}
+
+void setup_wifi() {
+  delay(50);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  if(!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  Serial.println("STA Failed to configure");
+  }
+  WiFi.begin(ssid, password);
+
+  // reset esp after 10 seconds if it is not connected to any wifi
+  int c=0;
+  while (WiFi.status() != WL_CONNECTED) {
+    blink_led(2,200); //blink LED twice (for 200ms ON time) to indicate that wifi not connected
+    delay(1000); //
+    Serial.print(".");
+    c=c+1;
+    if(c>10){
+        ESP.restart(); //restart ESP after 10 seconds
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
+
   Serial.setDebugOutput(true);
   Serial.println();
 
@@ -66,6 +158,7 @@ void setup() {
 
 /////////////////// MOTOR DRIVER ENDS  /////////////////////////////
 
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -81,22 +174,35 @@ void setup() {
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
   config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
+  config.xclk_freq_hz = 10000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
   } else {
-    config.frame_size = FRAMESIZE_QVGA;
-    config.jpeg_quality = 40;
-    config.fb_count = 1;
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
   }
+
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
@@ -105,29 +211,26 @@ void setup() {
     return;
   }
 
-// change cam config 
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_QVGA);
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
+  sensor_t *s = esp_camera_sensor_get();
+  Serial.print("Camera PID : ");
+  Serial.print(s->id.PID);
+  
+  s->set_framesize(s, FRAMESIZE_240X240);
+  
+  setup_wifi();
 
-  //s->set_xclk(s, LEDC_TIMER_0, xclk);
+//////////////////////////////////    WiFi INITIALIZATION COMPLETE    //////////////////////
 
-//////////////////  CAMERA CONFIG ENDS  /////////////////////////////
+  client.setServer(mqtt_server,mqttPort);//1883 is the default port for MQTT server
+  client.setCallback(callback);
 
-  if(!WiFi.config(local_ip, subnet)) {
-    Serial.println("Failed to configure Static IP");
-  } else {
-  Serial.println("Static IP configured!");
-  }
-  WiFi.begin(ssid, password);
+  pinMode(MQ2_PIN, INPUT);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+
+  Serial.println("Warming up the MQ2 sensor");
+  //delay(20000);  // wait for the MQ2 to warm up
+
+////////////////////////  SENSOR INITIALIZATION COMPLETE    ////////////////////////////////
 
   startCameraServer();
 
@@ -135,9 +238,56 @@ void setup() {
   Serial.print(WiFi.localIP());
   WiFiAddr = WiFi.localIP().toString();
   Serial.println("' to connect");
-
 }
 
 void loop() {
-}
+  delay(10000);
+    if (!client.connected()) {
+    connect_mqttServer();
+  }
+  client.loop();
 
+// MQ-2 sensor reading
+  int mq2_val = digitalRead(MQ2_PIN);
+
+  if (mq2_val == HIGH){
+    Serial.println("The gas is NOT present");
+    mq_value = "GAS is NOT Present";
+  }
+  else{
+    Serial.println("The gas is present");
+    mq_value = "GAS is Present";
+  }
+
+// DHT sensor reading
+  float dht_humidity = dht.readHumidity();
+  float dht_temp = dht.readTemperature();
+
+  delay(1000);
+ 
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(dht_humidity) || isnan(dht_temp)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  delay(100);
+  
+  long now = millis();
+  if (now - lastMsg > 4000) {
+    lastMsg = now;
+
+    char buffer1[10]; 
+    dtostrf(dht_temp, 1, 2, buffer1);
+    
+    char buffer2[10];  
+    dtostrf(dht_humidity,1,2, buffer2);
+
+    char buffer3[500]; 
+    memcpy(buffer3, mq_value,(size_t)sizeof(mq_value)* strlen(mq_value));
+
+    client.publish("esp32/sensor/bot", buffer1);
+    client.publish("esp32/sensor/bot", buffer2);
+    client.publish("esp32/sensor/bot", buffer3);
+  }
+}
